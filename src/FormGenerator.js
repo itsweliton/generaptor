ef02a70c4847f747b07391ab1e1d8404c12d486d
Weliton de Resende <welitonderesende@gmail.com>
2019-09-14 00:05:59 -0300

    innitial commit

---
+++ src/FormGenerator.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+
+const FormGenerator = schema => {
+
+  return <div>yey</div>
+}
+
+export default FormGenerator;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './index.css';
+import FormGenerator from './FormGenerator';
+
+export default FormGenerator;
