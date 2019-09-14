7e28be3445c29b6ee56adc67bb1f433e889ae6ac
Weliton de Resende <welitonderesende@gmail.com>
2019-09-14 02:09:58 -0300

    feat(Generate Forms) Implementing the basics, not working yet

--- src/FormGenerator.js
+++ src/FormGenerator.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
 
-const FormGenerator = schema => {
-
-  return <div>yey</div>
+const FormGenerator = ({ schema }) => {
+  if (!schema || !schema.fields) throw new Error('You must have a schema');
+  return (
+    <div>
+      <form>
+        {schema.fields.forEach(field => (
+          <>
+            <label for="name">{field.label}</label>
+            <input type="text" id={field.id} name="name" required minlength="4" maxlength="8" size="10" placeholder={field.placeholder}></input>
+          </>
+        ))}
+      </form>
+    </div>
+  );
 }
 
 export default FormGenerator;
---
+++ src/FormGenerator.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FormGenerator from './FormGenerator';
+import { shallow, mount } from 'enzyme';
+import { create } from 'react-test-renderer';
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+const schema = { 
+  fields: [
+    {
+      id: "le",
+      type: "input",
+      inputType: "text",
+      place: "Name",
+      placeholder: "Name",
+      model: "name"
+    }
+  ]
+}
+
+// describe("Form Generator Mounted", () => {
+//   it('Exports a valid component', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<FormGenerator schema={{fields: []}}/>, div);
+//     ReactDOM.unmountComponentAtNode(div);
+//   });
+
+//   test("Component is created", () => {
+//     const formGenerator = create(<FormGenerator schema={{fields: []}}/>);
+//     expect(formGenerator.toJSON()).toMatchSnapshot();
+//   });
+
+//   it("Component without schema throws error", () => {
+//     expect(() => mount(<FormGenerator />)).toThrowError(/^You must have a schema$/);
+//   });
+// })
+
+describe("Form Generator basic mounting", () => {
+  test("It should create a basic input", () => {
+    const wrapper = shallow(<FormGenerator schema={schema} />);
+    expect(wrapper).toMatchSnapshot()
+    expect(wrapper.find(`name`).props().placeholder).toBe("Name");
+  });
+});
